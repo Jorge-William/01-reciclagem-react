ca218b4578719354bf07d958c51d671a85138a10
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2023-11-13 16:14:14 -0300

    Feat: adicionando mais estilo e componentizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import { Post } from './Post'
-import { Header } from './componentes/Header'
-import './global.css'
+import { Post } from './componentes/Post';
+import { Header } from './componentes/Header';
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './componentes/SideBar';
 
 function App() {
-
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
-      <Post
-        autor="Jorge William"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, quas ea ipsa repellat, eos cum delectus, nostrum facilis quibusdam odio officiis illum labore doloribus ducimus at molestiae minus iste doloremque."
-      />
-      <Post
-        autor="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, quas ea ipsa repellat, eos cum delectus, nostrum facilis quibusdam odio officiis illum labore doloribus ducimus at molestiae minus iste doloremque."
-      />
-
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Diego Fernandes'
+            content='Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Possimus numquam eaque optio 
+            nesciunt obcaecati sapiente eius veritatis facere
+             iure, illo nihil dolores, aut illum excepturi, 
+             doloribus voluptates exercitationem iusto libero.'
+          />
+          <Post
+            author='Paula Reis'
+            content='Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Possimus numquam eaque optio 
+            nesciunt obcaecati sapiente eius veritatis facere
+             iure, illo nihil dolores, aut illum excepturi, 
+             doloribus voluptates exercitationem iusto libero.'
+          />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,9 @@
-import styles from './Header.module.css'
-import igniteLogo from '../assets/igniteLogo.svg'
+import styles from './Header.module.css';
+import igniteLogo from '../assets/igniteLogo.svg';
 export function Header() {
-    return (
-        <header className={styles.header}>
-            <strong ><img src={igniteLogo} alt="Logotipo" /></strong>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt='Logotipo' />
+    </header>
+  );
 }
