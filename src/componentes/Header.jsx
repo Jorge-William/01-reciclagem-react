54d89d3a88c1149aa663a2732ad81e8e69dfc0e7
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2023-11-13 12:29:34 -0300

    Feat: header configurado logo adicionado

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,9 @@
 import styles from './Header.module.css'
-
+import igniteLogo from '../assets/igniteLogo.svg'
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong >Ignite Feed</strong>
+            <strong ><img src={igniteLogo} alt="Logotipo" /></strong>
         </header>
     )
 }
