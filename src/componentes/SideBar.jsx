3b74ea3e8b1aa1893f868b3e9bab481089187083
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2023-11-15 17:05:38 -0300

    Feat: sidebar  layout

--- src/componentes/SideBar.jsx
+++ src/componentes/SideBar.jsx
@@ -1,4 +1,5 @@
 import styles from './SideBar.module.css';
+import { Pencil } from '@phosphor-icons/react';
 
 export function Sidebar() {
   return (
@@ -8,11 +9,19 @@ export function Sidebar() {
         src='https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src='https://avatars.githubusercontent.com/u/19416864?v=4'
+          alt='foto de perfil'
+        />
         <strong>Jorge William</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href='#'>Editar seu perfil</a>{' '}
+        <a href='#'>
+          <Pencil size={20} />
+          Editar seu perfil
+        </a>{' '}
       </footer>
     </aside>
   );
